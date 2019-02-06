65e5b13b5631bb3fec57445c9cd6a1d687503017
JacobEberhardt <jacob.eberhardt@tu-berlin.de>
2019-02-06 12:51:53 +0100

    intermediary version. Not working yet

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn main() {
     let unparsed_file = fs::read_to_string("zoksample.code").expect("cannot read file");
 
     let parse = ZoKratesParser::parse(Rule::file, &unparsed_file).expect("unsuccessful parse"); // unwrap the parse result
-    println!("{:?}", parse);
+    println!("{:#?}", parse);
 }
 
 #[cfg(test)]
@@ -25,15 +25,24 @@ mod tests {
 
     mod examples {
         use super::*;
+        extern crate glob;
+        use glob::glob;
+
         #[test]
         fn examples_dir() {
-            // TODO: Traverse examples dir recursively and read all .code files
-            let mut file = fs::File::open("examples/brackets.code").unwrap();
-            let mut data = String::new();
-
-            file.read_to_string(&mut data).unwrap();
-
-            ZoKratesParser::parse(Rule::file, &data).unwrap();
+            // Traverse all .code files in examples dir
+            for entry in glob("examples/**/*.code").expect("Failed to read glob pattern") {
+                match entry {
+                    Ok(path) => {
+                        println!("Parsing {:?}", path.display());
+                        let mut file = fs::File::open(path).unwrap();
+                        let mut data = String::new();
+                        file.read_to_string(&mut data).unwrap();
+                        ZoKratesParser::parse(Rule::file, &data).unwrap();
+                    }
+                    Err(e) => println!("{:?}", e),
+                }
+            }
         }
     }
 
@@ -60,6 +69,24 @@ mod tests {
                 positives: vec![Rule::identifier],
                 negatives: vec![],
                 pos: 0
+            };
+        }
+
+        #[test]
+        fn parse_for_loop() {
+            parses_to! {
+                parser: ZoKratesParser,
+                input : "for field i in 0..3 do \n c = c + a[i] \n endfor",
+                rule: Rule::statement,
+                tokens: [
+                    iteration_statement(0, 42),
+                    type_name(4,9),
+                    identifier(10,11),
+                    constant(15,16),
+                    constant(18,19),
+                    statement(26,41),
+
+                ]
             };
         }
 
